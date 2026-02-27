9724e3e1f191c0d3872ca1280936739de5797f93
Malik-Umer-Mehboob <umermehboob563@gmail.com>
2026-02-26 23:30:01 -0800

    Delete backend directory

--- backend/routes/ticketRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getMyTickets, downloadTicket } = require('../controllers/ticketController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.get('/my-tickets', protect, getMyTickets);
-router.get('/:id/download', protect, downloadTicket);
-
-module.exports = router;
